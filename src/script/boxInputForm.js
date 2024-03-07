76c5c746873bf4e2aacc3155cd79b13e260ad148
Ali Nurdin <alinurdin159@gmail.com>
2024-03-07 07:33:30 +0700

    fix box

--- src/script/boxInputForm.js
+++ src/script/boxInputForm.js
@@ -7,9 +7,33 @@ boxInputSubmit.addEventListener('click', () => {
   boxContainer.innerHTML = ''
   boxContainer.style.display = 'none'
 
+  if (boxTotal <= 12) {
+    boxContainer.style.gridTemplateColumns = 'repeat(12, minmax(auto, 1fr))'
+  } else if (boxTotal <= 24) {
+    boxContainer.style.gridTemplateColumns = 'repeat(24, minmax(auto, 1fr))'
+  } else if (boxTotal <= 36) {
+    boxContainer.style.gridTemplateColumns = 'repeat(36, minmax(auto, 1fr))'
+  } else {
+    boxContainer.style.gridTemplateColumns = 'repeat(48, minmax(auto, 1fr))'
+  }
+
   for (let index = 0; index < boxTotal; index++) {
     boxContainer.innerHTML +=  `<div class="box"></div>`
   }
+
+  const boxes = document.querySelectorAll('.box');
+
+  boxes.forEach(box => {
+    if (boxTotal <= 12) {
+      box.style.height = '28px'
+    } else if (boxTotal <= 24) {
+      box.style.height = '12px'
+    } else if (boxTotal <= 36) {
+      box.style.height = '6px'
+    } else {
+      box.style.height = '4px'
+    }
+  })
 
   boxContainer.style.display = 'grid'
 })
